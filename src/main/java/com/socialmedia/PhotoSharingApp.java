aca887b1913b054b2f135bf56ccecf931afa2abc
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-05 09:10:33 -0300

    Initial commit

---
+++ src/main/java/com/socialmedia/PhotoSharingApp.java
@@ -0,0 +1,12 @@
+package com.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSharingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSharingApp.class, args);
+    }
+}
